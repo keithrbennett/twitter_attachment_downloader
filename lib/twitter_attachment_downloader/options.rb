12001f7f4f9b33c4ca712da182cfe6cb871e2560
Keith Bennett <keithrbennett@gmail.com>
2017-02-19 15:36:58 +0700

    Extracted Options and Downloader classes & fixed a couple of bugs.

---
+++ lib/twitter_attachment_downloader/options.rb
@@ -0,0 +1,52 @@
+# Encapsulates and manages program options, including getting them from the command line.
+
+require 'optparse'
+
+class Options
+
+  attr_writer :archive_root_dir, :include_retweets
+
+  def self.from_command_line
+
+    option_parser = nil
+    output_help_and_terminate = -> { puts option_parser; puts; exit }
+
+    options = Options.new
+
+    OptionParser.new do |opts|
+
+      option_parser = opts
+
+      opts.banner = "Usage: twitter-attachment-downloader [options]"
+
+      opts.on('-d', '--archive_root_dir dir', 'Root of Twitter archive directory (default: .)') do |value|
+        options.archive_root_dir = value
+      end
+
+      opts.on('-r', '--[no-]include_retweets', 'Download attachments to retweets') do |value|
+        options.include_retweets = value
+      end
+
+      opts.on_tail("-h", "--help", "Show this message") do
+        output_help_and_terminate.()
+      end
+
+    end.parse!
+    options
+  end
+
+
+  def archive_root_dir
+    @archive_root_dir ||= '.'
+  end
+
+
+  def tweet_index_filespec
+    File.join(archive_root_dir, 'data', 'js', 'tweet_index.js')
+  end
+
+
+  def tweet_dirspec
+    File.join(archive_root_dir, 'data', 'js', 'tweets')
+  end
+end
