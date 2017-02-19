b9dae905f58a498d616f2b42a8fa6bbccee75b24
Keith Bennett <keithrbennett@gmail.com>
2017-02-19 16:05:28 +0700

    Improve implementation of ExitCodes.

--- lib/twitter_attachment_downloader/exit_codes.rb
+++ lib/twitter_attachment_downloader/exit_codes.rb
@@ -1,15 +1,22 @@
 # Exit Codes for code throughout the application.
 
-module ExitCodes
+class ExitCodes
+
+  CODES =     {
+      archive_root_dir_missing:   -1,
+      tweet_master_index_missing: -2,
+      download_failed:            -3,
+      index_file_missing:         -4,
+      keyboard_interrupt:         -5
+  }
 
   # e.g. ExitCodes.for(:download_failed) => -3
   def self.for(reason)
-    {
-        archive_root_dir_missing:   -1,
-        tweet_master_index_missing: -2,
-        download_failed:            -3,
-        index_file_missing:         -4,
-        keyboard_interrupt:         -5
-    }[reason]
+    if CODES.keys.include?(reason)
+      CODES[reason]
+    else
+      $stderr.puts "ExitCodes: Unknown reason: #{reason.inspect}"
+      -9999
+    end
   end
 end
